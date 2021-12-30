07be45547eecd568832a55736e362b188a28f088
MMue99 <m_mue011@uni-muenster.de>
2021-12-30 18:20:43 +0100

    Indicator Variables sind da

--- Assignment R.R
+++ Assignment R.R
@@ -8,13 +8,18 @@
 #Lionel Lütgering
 
 
+#Libraries --------
 #install.packages("haven")
 library (haven)
 library(ggplot2)
+library(tidyverse)
+
+#Data Import --------
 carbontax_data <- read_dta("Data/carbontax_fullsample_data.dta")
 CO2_data <- read_dta("Data/CO2_Graph.dta")
 
 
+#Nr. 2 ------
 figure_3 <- ggplot2::ggplot(data = CO2_data,
                             mapping = aes(year)
                             )+
@@ -27,7 +32,21 @@ figure_3 <- ggplot2::ggplot(data = CO2_data,
   geom_vline(xintercept = 1990, linetype = "dotted",size=1)
 
 
-  
-
-
 figure_3
+
+
+#Nr. 5 -----
+##Generating Indicator Variables---------
+
+carbontax_working <- carbontax_data
+carbontax_working <- carbontax_working %>%
+  mutate(sweden_indicator = ifelse(country == "Sweden", 1, 0))
+
+carbontax_working <- carbontax_working %>%
+  mutate(post_indicator = ifelse(year >= 1990, 1, 0))
+
+##Creating Regressions -----
+
+#reg1 <- lm(CO2_transport_capita ~ . + post_indicator*sweden_indicator, data = carbontax_working)
+#summary(reg1)
+
