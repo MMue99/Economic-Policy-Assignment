2962e7b669d2729d049f5bf1db1fd37c6b417a15
SturmFa <f_stur01@uni-muenster.de>
2021-12-30 10:00:18 +0100

    sieht noch echt nicht so geil aus, aber die daten sind drin

--- Assignment R.R
+++ Assignment R.R
@@ -1,12 +1,33 @@
 #Assignment for Economic Policy, Winter Semester 21/22
 
-'''
 
-Authors:
+#Authors:
 
-Marvin Müller
-Fabian Sturmberg
-Lionel Lütgering
+#Marvin MÃ¼ller
+#Fabian Sturmberg
+#Lionel LÃ¼tgering
 
-'''
 
+#install.packages("haven")
+library (haven)
+library(ggplot2)
+carbontax_data <- read_dta("Data/carbontax_fullsample_data.dta")
+CO2_data <- read_dta("Data/CO2_Graph.dta")
+
+
+figure_3 <- ggplot2::ggplot(data = CO2_data,
+                            mapping = aes(year)
+                            )+
+  geom_line(aes(y=CO2_OECD), linetype = "dashed")+
+  geom_line(aes(y=CO2_Sweden))+
+  coord_cartesian(xlim = c(1960,2005), ylim = c(0.0,3.0),expand = FALSE)+
+  xlab("")+
+  ylab("Metric tons per capita (CO2 from transport)")+
+  labs(title = "CARBON TAXES AND CO2 EMISSIONS")+
+  geom_vline(xintercept = 1990, linetype = "dotted",size=1)
+
+
+  
+
+
+figure_3
