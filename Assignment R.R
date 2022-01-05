4aa282319b6403023d4beecb66de541c6a7ed8a1
SturmFa <f_stur01@uni-muenster.de>
2022-01-05 09:58:08 +0100

    Plots

--- Assignment R.R
+++ Assignment R.R
@@ -44,12 +44,11 @@ carbontax_data_sample <- carbontax_data%>%
   filter(country=="Sweden" |country=="Denmark"|country=="Finland"|
            country=="Norway"|country=="Germany"|country=="France")
 
-summary(carbontax_data_sample)
-glimpse(carbontax_data_sample)
 
 countries <- c("Sweden","Denmark","Finland","Norway","Germany","France")
 
 
+#Data for Table of pre- and post-treatment averages
 carbontax_data_sample_pre <- carbontax_data_sample %>%
   filter(year <= 1989 & year >= 1980)
 
@@ -58,15 +57,13 @@ carbontax_data_sample_post <- carbontax_data_sample %>%
 
 avg_table_pre <- matrix(c(1:36), ncol=6, byrow=TRUE)
 colnames(avg_table_pre) <- c('Sweden','Denmark','Finland','Norway','Germany','France')
-rownames(avg_table_pre) <- c('CO2 from transport','GDP per capita','Gasoline consumption per capita', 
-                         'Motor vehicles (per 1,000 people', 'Urban population','Population density')
-#avg_table_pre <- as.table(avg_table_pre)
+rownames(avg_table_pre) <- c('CO2 from transport (in metric tons)','GDP per capita (PPP, 2005)','Gasoline consumption per capita (in kilograms of oil equivalent)', 
+                            'Motor vehicles (per 1,000 people)', 'Urban population (as percentage of total population)','Population density')
 
 avg_table_post <- matrix(c(1:36), ncol=6, byrow=TRUE)
 colnames(avg_table_post) <- c('Sweden','Denmark','Finland','Norway','Germany','France')
-rownames(avg_table_post) <- c('CO2 from transport','GDP per capita','Gasoline consumption per capita', 
-                             'Motor vehicles (per 1,000 people', 'Urban population','Population density')
-#avg_table_post <- as.table(avg_table_post)
+rownames(avg_table_post) <- c('CO2 from transport (in metric tons)','GDP per capita (PPP, 2005)','Gasoline consumption per capita (in kilograms of oil equivalent)', 
+                             'Motor vehicles (per 1,000 people)', 'Urban population (as percentage of total population)','Population density')
 
 #pre
 swe_df_pre <- carbontax_data_sample_pre%>%
@@ -104,7 +101,7 @@ for(i in 4:9){
     avg_table_pre[i-3,4] = mean(nor_df_pre[[i]])
     avg_table_pre[i-3,5] = mean(ger_df_pre[[i]])
     avg_table_pre[i-3,6] = mean(fra_df_pre[[i]])
-    }
+}
 
 #post
 for(i in 4:9){
@@ -116,19 +113,60 @@ for(i in 4:9){
   avg_table_post[i-3,6] = mean(fra_df_post[[i]])
   }
 
+#AVG's from sample data from 1980 - 1989 (pre treatment)
+avg_table_pre
+#AVG's from sample data from 1990 - 2000 (post treatment)
+avg_table_post
 
 
+#Plot Sweden and Average of all other countries in sample
+
+#Create Data Frame with mean of all other countries but Sweden
+
+sample_df <- as.data.frame(do.call(cbind,carbontax_data_sample))
+sapply(sample_df, class)
+# defining the vector of columns to convert to numeric
+vec <- c(3:9)
+# apply the conversion on columns
+sample_df[ , vec] <- apply(sample_df[ , vec,drop=F], 2,           
+                            function(x) as.numeric(as.character(x)))
+# indicating the data type of each variable
+sapply(sample_df, class)
+
+sample_avg <- sample_df%>%
+  filter(country != "Sweden")%>%
+  group_by(year)%>%
+  do(mutate(., mean_CO2_transport = mean(.$CO2_transport_capita)))
+
+swe_df <- sample_df%>%
+  filter(country == "Sweden")
+
+OECD_sample <- data.frame(year = sample_avg$year,country = rep("Sample Countries",230),CO2_transport_capita = sample_avg$mean_CO2_transport)
+OECD_sample <- OECD_sample[!duplicated(OECD_sample), ]
+
+
+#Plot all countries' data from Sample + smooth (?)
 task_3_plot_1 <- ggplot2::ggplot(data=carbontax_data_sample,
                                  aes(year,CO2_transport_capita,
                                      group = country,
                                      color = country))+
-  geom_smooth(se=F,linetype = "dashed",size=0.15)+#dashed line that shows prediction of regression for each country
+  #geom_smooth(se=F,linetype = "dashed",size=0.15)+#dashed line that shows prediction of regression for each country
   geom_line()+#straight line that connects data points of each country
   geom_vline(xintercept = 1990, linetype = "dotted",size=0.2)#Sweden Treatment
 
 task_3_plot_1
 
+#Plot with Sweden and Rest of Sample Countries
 
+task_3_plot_2 <- ggplot2::ggplot(data = OECD_sample,
+                                 aes(year, CO2_transport_capita,
+                                     group = country,
+                                     color = country))+
+  geom_line()+
+  geom_line(data = swe_df, aes(year, CO2_transport_capita))+
+  geom_vline(xintercept = 1990, linetype = "dotted",size=0.2)#Sweden Treatment
+
+task_3_plot_2
 
 
 #Nr. 5 -----
